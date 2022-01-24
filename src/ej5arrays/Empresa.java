ce76ee6233af3a364dc9328997de6c5f623432e0
dam <dam@203P20>
2022-01-24 09:24:12 +0100

    ejercicio completado metodo busqueda añadido

--- src/ej5arrays/Empresa.java
+++ src/ej5arrays/Empresa.java
@@ -23,10 +23,10 @@ public class Empresa {
         clientes = new Cliente[nClientes];
         descuentos = new Descuento[]{
             new Descuento(30, 0.1f),
-            new Descuento(90, 0.1f),
-            new Descuento(120, 0.1f),
-            new Descuento(180, 0.1f),
-            new Descuento(Integer.MAX_VALUE, 0.1f)
+            new Descuento(90, 0.07f),
+            new Descuento(120, 0.05f),
+            new Descuento(180, 0.02f),
+            new Descuento(Integer.MAX_VALUE, 0f)
         };
     }
 
@@ -44,22 +44,53 @@ public class Empresa {
             clientes[ncliente] = new Cliente(cif, nombre, fechaFra, dven, importe);
         }
     }
-    public void informe(){
-        Fecha hoy=new Fecha();
+
+    public void informe() {
+        Fecha hoy = new Fecha();
+        float descuento, importeNeto;
+        int pos;
         System.out.println("\t\t\t\tINFORME DE FACTURAS");
-        System.out.println("Fecha:"+ hoy.fechaCompleta());
+        System.out.println("Fecha:" + hoy.fechaCompleta());
         System.out.println("CIF \t NOMBRE \t FECHA \t FACTURA \t IMPORTE BRUTO \t FECHA VECIMIENTO \t IMPORTE NETO");
-        for(int ncliente=0; ncliente<clientes.length; ncliente++){
-            Fecha vencimiento= new Fecha(
+        for (int ncliente = 0; ncliente < clientes.length; ncliente++) {
+            pos = busqueda(clientes[ncliente].getDiasVen());
+            if (pos == -1) {
+                descuento = 0;
+            } else {
+                descuento = clientes[ncliente].getImporte() * descuentos[pos].getDescuento();
+            }
+            importeNeto = clientes[ncliente].getImporte() - descuento;
+            Fecha vencimiento = new Fecha(
                     clientes[ncliente].getFechaFra().getDia(),
                     clientes[ncliente].getFechaFra().getMes(),
                     clientes[ncliente].getFechaFra().getAnno());
             vencimiento.calcularVencimiento(clientes[ncliente].getDiasVen());
             System.out.print(clientes[ncliente].getCif());
-            System.out.print("\t"+clientes[ncliente].getNombre());
-            System.out.print("\t"+clientes[ncliente].getFechaFra().fechaCompleta());
-            System.out.print("\t"+clientes[ncliente].getImporte());
-            System.out.print("\t"+ vencimiento.fechaCompleta());
+            System.out.print("\t" + clientes[ncliente].getNombre());
+            System.out.print("\t" + clientes[ncliente].getFechaFra().fechaCompleta());
+            System.out.print("\t" + clientes[ncliente].getImporte());
+            System.out.print("\t" + vencimiento.fechaCompleta());
+            System.out.print("\t" + importeNeto);
         }
+    }
+
+    /**
+     *
+     * @param valor del elemento a buscar
+     * @return posicion del valor encontrad
+     */
+    public int busqueda(int valor) {
+        int pos = 0;
+        boolean encontrado = false;
+        while (!encontrado && pos < descuentos.length) {
+            if (valor > descuentos[pos].getLimite()) 
+                pos++;
+            else 
+                encontrado = true;
+            }
+            if (!encontrado) {
+                pos = -1;
+        }
+        return pos;
     }
 }
